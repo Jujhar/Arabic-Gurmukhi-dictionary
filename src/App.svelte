705a345c74b71c424bb851aefad9cff4f56a7291
Jujhar Singh <jujhar.pannu@gmail.com>
2022-06-02 20:32:55 -0700

    update logic + add words

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,60 @@
 let index = 0;
 $: place = index;
 
-let word = {"data":[{"id":1,"attributes":{"definition":"to please","createdAt":"2022-05-16T01:01:40.251Z","updatedAt":"2022-05-16T02:10:12.283Z","MaleorPresent":"ਯੁਜਬੋ","FemaleorPast":"ਆਜ਼ਬਾ","Type":"Past / Present"}},{"id":2,"attributes":{"definition":"be amazed","createdAt":"2022-05-16T01:11:51.629Z","updatedAt":"2022-05-16T02:10:01.285Z","MaleorPresent":"ਤਾਜ਼ਾਬਾ","FemaleorPast":"ਤਾਜ਼ਾਬੂ","Type":"Past / Present"}},{"id":3,"attributes":{"definition":"amazing/spectacular","createdAt":"2022-05-16T01:15:40.897Z","updatedAt":"2022-05-16T02:10:06.824Z","MaleorPresent":"ਮੁਦਿਲ","FemaleorPast":"ਮੁਦਿਲੋਨ","Type":"Singular / Plural"}}],"meta":{"pagination":{"page":1,"pageSize":25,"pageCount":1,"total":3}}}
+let word = {"data":[{"id":1,"attributes":{"definition":"to please","createdAt":"2022-05-16T01:01:40.251Z","updatedAt":"2022-05-16T02:10:12.283Z","MaleorPresent":"ਯੁਜਬੋ","FemaleorPast":"ਆਜ਼ਬਾ","Type":"Past / Present","DateAdded":null,"Lang":null}},{"id":2,"attributes":{"definition":"be amazed","createdAt":"2022-05-16T01:11:51.629Z","updatedAt":"2022-05-16T02:10:01.285Z","MaleorPresent":"ਤਾਜ਼ਾਬਾ","FemaleorPast":"ਤਾਜ਼ਾਬੂ","Type":"Past / Present","DateAdded":null,"Lang":null}},{"id":3,"attributes":{"definition":"amazing/spectacular","createdAt":"2022-05-16T01:15:40.897Z","updatedAt":"2022-05-16T02:10:06.824Z","MaleorPresent":"ਮੁਦਿਲ","FemaleorPast":"ਮੁਦਿਲੋਨ","Type":"Singular / Plural","DateAdded":null,"Lang":null}},{"id":4,"attributes":{"definition":"reverence / jaleel - majesty","createdAt":"2022-05-22T23:59:27.468Z","updatedAt":"2022-05-22T23:59:27.468Z","MaleorPresent":"ਅਜਿਾਲਾ","FemaleorPast":"ਜਲੀਲ","Type":"Male / Female","DateAdded":null,"Lang":null}},{"id":5,"attributes":{"definition":"sacred","createdAt":"2022-05-23T00:04:02.554Z","updatedAt":"2022-05-23T00:04:02.554Z","MaleorPresent":"ਮੁਕਾਦਿਸ","FemaleorPast":"ਮੁਕਾਦਿਸੋਲ","Type":"Male / Female","DateAdded":null,"Lang":null}},{"id":6,"attributes":{"definition":"beard","createdAt":"2022-06-02T17:23:16.190Z","updatedAt":"2022-06-02T17:23:16.190Z","MaleorPresent":"ਰੀਛ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Persian"}},{"id":7,"attributes":{"definition":"sand","createdAt":"2022-06-02T17:27:28.610Z","updatedAt":"2022-06-02T17:27:41.464Z","MaleorPresent":"ਰਿਗ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Persian"}},{"id":8,"attributes":{"definition":"food / nourishment","createdAt":"2022-06-02T17:35:46.550Z","updatedAt":"2022-06-02T18:49:46.106Z","MaleorPresent":"ਤਾਮ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":9,"attributes":{"definition":"diet / nutrition","createdAt":"2022-06-02T17:36:57.808Z","updatedAt":"2022-06-02T18:50:01.395Z","MaleorPresent":"ਰਿਦਾਓਣ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":10,"attributes":{"definition":"fire","createdAt":"2022-06-02T18:20:55.648Z","updatedAt":"2022-06-02T18:20:55.648Z","MaleorPresent":"ਨਾਰ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":11,"attributes":{"definition":"separate, detach, disconnect, break, dismiss","createdAt":"2022-06-02T18:22:11.950Z","updatedAt":"2022-06-02T18:22:11.950Z","MaleorPresent":"ਫਸਲੁਣ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":12,"attributes":{"definition":"enthusiasm, intense interest, heartiness","createdAt":"2022-06-02T18:23:23.819Z","updatedAt":"2022-06-02T18:23:23.819Z","MaleorPresent":"ਹੁਮਾਸ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":13,"attributes":{"definition":"fuel","createdAt":"2022-06-02T18:25:43.270Z","updatedAt":"2022-06-02T18:25:43.270Z","MaleorPresent":"ਵਾਕੋਡੂਣ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":14,"attributes":{"definition":"no","createdAt":"2022-06-02T18:29:39.341Z","updatedAt":"2022-06-02T18:29:39.341Z","MaleorPresent":"ਲਾ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":15,"attributes":{"definition":"good, welfare-giving, benevolent, charitable,","createdAt":"2022-06-02T18:33:53.992Z","updatedAt":"2022-06-02T18:33:53.992Z","MaleorPresent":"ਕਾਈਰੁਨ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":16,"attributes":{"definition":"clean","createdAt":"2022-06-02T18:38:00.313Z","updatedAt":"2022-06-02T18:38:00.313Z","MaleorPresent":"ਨਾਜ਼ੀਫ","FemaleorPast":"ਨੁਜ਼ਾਫਾਤ","Type":"Singular / Plural","DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":17,"attributes":{"definition":"put, set, position, lay, design","createdAt":"2022-06-02T18:40:41.970Z","updatedAt":"2022-06-02T18:40:41.970Z","MaleorPresent":"ਵਾਡਾ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":18,"attributes":{"definition":"work, action, business, job, labor","createdAt":"2022-06-02T18:41:45.526Z","updatedAt":"2022-06-02T18:41:45.526Z","MaleorPresent":"ਆਮੁਲਣ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":19,"attributes":{"definition":"request, demand, order, seek, require","createdAt":"2022-06-02T18:42:50.532Z","updatedAt":"2022-06-02T18:42:50.532Z","MaleorPresent":"ਤਾਲਿਬ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":20,"attributes":{"definition":"task, mission, job, function, assignment","createdAt":"2022-06-02T18:43:43.227Z","updatedAt":"2022-06-02T18:43:43.227Z","MaleorPresent":"ਮੋਹਿਮ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}},{"id":21,"attributes":{"definition":"function, job, position, office, service","createdAt":"2022-06-02T18:44:55.981Z","updatedAt":"2022-06-02T18:45:43.400Z","MaleorPresent":"ਵਾਬੀਫਾਤਣ","FemaleorPast":null,"Type":null,"DateAdded":"2022-06-02T07:00:00.000Z","Lang":"Arabic"}}],"meta":{"pagination":{"page":1,"pageSize":25,"pageCount":1,"total":21}}}
 
-function indexIs(i){
-  console.log('index is'+index);
-  index = index+i;
-  console.log('index is now'+index);
-}
 
-//prevWord0 = word[index-3];
-//prevWord1 = x;
-//prevWord2 = x;
 let nextWord0 = word.data[index+1].attributes.MaleorPresent;
 let nextWord1 = word.data[index+2].attributes.MaleorPresent;
-//nextWord2 = x;
-//nextWord3 = x;
+let nextWord2 = word.data[index+3].attributes.MaleorPresent;
+let nextWord3 = word.data[index+4].attributes.MaleorPresent;
+let prevWord2 = '';
+let prevWord1 = '';
+let prevWord0 = '';
 
-console.log(word);
+if (index > 0) {
+  prevWord2 = word.data[index-1].attributes.MaleorPresent;
+}
+if (index > 1) {
+  prevWord1 = word.data[index-2].attributes.MaleorPresent;
+}
+if (index > 2) {
+  prevWord0 = word.data[index-3].attributes.MaleorPresent;
+}
 
+function indexIs(i){
+  if (i == -1) {
+    index = place-1;
+    nextWord0 = word.data[index].attributes.MaleorPresent;
+    nextWord1 = word.data[index+1].attributes.MaleorPresent;
+    nextWord2 = word.data[index+2].attributes.MaleorPresent;
+    nextWord3 = word.data[index+3].attributes.MaleorPresent;
+    if (index > 0) {
+      prevWord2 = word.data[index-1].attributes.MaleorPresent;
+    }
+    if (index > 1) {
+      prevWord1 = word.data[index-2].attributes.MaleorPresent;
+    }
+    if (index > 2) {
+      prevWord0 = word.data[index-3].attributes.MaleorPresent;
+    }
+  } else {
+    index = place+i;
+    nextWord0 = word.data[index+1].attributes.MaleorPresent;
+    nextWord1 = word.data[index+2].attributes.MaleorPresent;
+    nextWord2 = word.data[index+3].attributes.MaleorPresent;
+    nextWord3 = word.data[index+4].attributes.MaleorPresent;
+    if (index > 0) {
+      prevWord2 = word.data[index-1].attributes.MaleorPresent;
+    }
+    if (index > 1) {
+      prevWord1 = word.data[index-2].attributes.MaleorPresent;
+    }
+    if (index > 2) {
+      prevWord0 = word.data[index-3].attributes.MaleorPresent;
+    }
+  }
+}
 
 </script>
 
@@ -71,9 +107,23 @@ http://www.branah.com/thai
 <!-- The Word -->
 <big><big>
 <h1>
-  <big><span style="color:#43C6DB">{word.data[index].attributes.MaleorPresent} / {word.data[index].attributes.FemaleorPast}</span></big>
+  <big><span style="color:#43C6DB">{word.data[index].attributes.MaleorPresent}
+  {#if word.data[index].attributes.FemaleorPast}
+                      / {word.data[index].attributes.FemaleorPast}
+                      {/if}
+
+
+  </span></big>
   <br />
-  <small><span style="color:grey;font-size:0.5em">({word.data[index].attributes.Type})</span></small>
+  <small><span style="color:grey;font-size:0.5em">
+  {#if word.data[index].attributes.Type}
+                    ({word.data[index].attributes.Type})
+  {/if}
+
+    {#if word.data[index].attributes.Lang == 'Persian'}
+                      <u>Persian</u>
+    {/if}
+  </span></small>
 </h1></big></big>
 <br />
 
@@ -96,29 +146,39 @@ http://www.branah.com/thai
 
   <p>
 
-   <span style="opacity:0.1">Prev  -</span>
+   <span on:click={()=>indexIs(-1)} style="opacity:0.1;cursor:pointer;">Prev  -</span>
 
 
 
 
 
-										<a href='index.php?laphaza=ਰਣਵਾਸ' style='color:#996666'>,</a>
-                     <span style='display:inline-block;text-align:center;'><a href='index.php?laphaza=ਰਤਨ ਜੋਤ' style='color:#996666'>,</a>
+                    <span class="next_btns">
+                    {#if index >= 2}
+                       <a style='color:#996666' on:click={()=>indexIs(-2)}>{prevWord1},</a>
+                     {/if}
+                     </span>
 
 
-                     </span> <span style='display:inline-block;color:#666666;text-align:center;'>{word.data[index].attributes.MaleorPresent},</span>
+
+                    <span class="next_btns" style='display:inline-block;text-align:center;'>
+                     {#if index >= 1}
+                      	<a style='color:#996666' on:click={()=>indexIs(-1)}>{prevWord2},</a>
+                      {/if}</span>
 
 
-                    <span style='display:inline-block;padding-left:15px;padding-right:5px;text-align:center;'>
+                      <span class="next_btns" style='display:inline-block;color:#666666;text-align:center;'>{word.data[index].attributes.MaleorPresent},</span>
+
+
+                    <span class="next_btns" style='display:inline-block;padding-left:15px;padding-right:5px;text-align:center;'>
 
                     <a on:click={()=>indexIs(1)}>{nextWord0},</a></span>
 
-                    <a on:click={()=>indexIs(0)}>{nextWord1}</a>,
+                    <span class="next_btns"><a on:click={()=>indexIs(1)}>{nextWord1}</a></span>,
 
-                    <a href='index.php?laphaza=ਰਾਈ'></a>,
+                    <!--<a href='index.php?laphaza=ਰਾਈ'></a>,
 
-                    <a href='index.php?laphaza=ਰਾਹ'></a>
-        <span style="opacity:0.1">- Next</span>
+                    <a href='index.php?laphaza=ਰਾਹ'></a>-->
+        <span style="opacity:0.1;cursor: pointer;" on:click={()=>indexIs(1)}>- Next</span>
  </p>
 
 
@@ -143,6 +203,11 @@ body {
 }
 .twordspt{
 font-size:1.5em;
+}
+.next_btns {
+  max-width:75px;
+  text-align: center;
+  cursor: pointer;
 }
 #keyboardContainer{
 padding:5px;
